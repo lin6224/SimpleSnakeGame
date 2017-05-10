77c6c7bb86dcd873a804a450b685521036c00e18
Jianfei(Jae) Lin <lin6224@hotmail.com>
2017-05-09 23:47:33 -0400

    Add project files.

---
+++ Snake/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// to set a bool gameOver, then to make a while loop to draw snake in the grapic
+bool gameOver;
+
+// Setup method to start the snake game
+void Setup()
+{
+	gameOver = false;
+}
+
+void Draw()
+{
+
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+int main()
+{
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+	}
+	return 0;
+}
